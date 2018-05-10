174b3beb7fa6e47e25b11a36644a7b3ad33b3c92
Manuel Limones <mlimones@pensare.mx>
2018-05-09 23:12:28 -0500

    spawn openfire 4.2.3 container

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --name openfire -d --restart=always \
+docker run --name openfire -d\
     --publish 9090:9090 --publish 5222:5222 --publish 7777:7777 \
-    sameersbn/openfire:3.10.3-19
+    gizmotronic/openfire:4.2.3
